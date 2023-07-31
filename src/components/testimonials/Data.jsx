4e5b86122c47df4c0ff67533e0d329ef51833ac3
RahmadahyaWeb <thelastpc24@gmail.com>
2023-07-31 22:20:08 +0700

    change border radius

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,8 +1,16 @@
 import Image1 from "../../assets/testimonial1.png";
 import Image2 from "../../assets/testimonial2.png";
 import Image3 from "../../assets/testimonial3.png";
+import Image4 from "../../assets/testimonials4.jpg";
 
 export const Data = [
+  {
+    id: 4,
+    image: Image4,
+    title: "Mira Arlina",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
   {
     id: 1,
     image: Image1,
@@ -17,6 +25,7 @@ export const Data = [
     description:
       "A really good job, all aspects of the project were followed step by step and with good results.",
   },
+
   {
     id: 3,
     image: Image3,
