2437bf314f3a7be2e0d3d8169304a0acb044855b
RahmadahyaWeb <thelastpc24@gmail.com>
2023-08-09 20:25:19 +0700

    fix minor bugs

--- src/components/preloader/Preloader.jsx
+++ src/components/preloader/Preloader.jsx
@@ -4,20 +4,22 @@ import "./Preloader.css";
 
 const Preloader = () => {
   return (
-    <div className="preloader">
-      <ThreeCircles
-        height="100"
-        width="100"
-        color="#000000"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-        ariaLabel="three-circles-rotating"
-        outerCircleColor=""
-        innerCircleColor=""
-        middleCircleColor=""
-      />
-    </div>
+    <section className="preloader">
+      <div>
+        <ThreeCircles
+          height="100"
+          width="100"
+          color="#000000"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="three-circles-rotating"
+          outerCircleColor=""
+          innerCircleColor=""
+          middleCircleColor=""
+        />
+      </div>
+    </section>
   );
 };
 
